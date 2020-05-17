08ef482ccbce4eb4c70e29ed99e16c430c07027a
Bill Fan <1472451850@qq.com>
2020-05-17 02:29:40 -0400

    Added semantic UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { AppContainer } from 'react-hot-loader'
+import { AppContainer } from 'react-hot-loader';
+import { Button, Grid, GridColumn } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
 
 function App() {
   return (
     <AppContainer>
       <div className="App">
         <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload!!!!
-          </p>
+          <Grid columns="2" divided relxaed>
+            <GridColumn>
+              <p>
+              Welcome to the Ultimate <code>Skribbl.io</code> Experience
+              </p>
+            </GridColumn>
+            
+            <GridColumn>
+              <Button primary> Click here to get started </Button>
+            </GridColumn>
+          </Grid>
         </header>
       </div>
     </AppContainer>
